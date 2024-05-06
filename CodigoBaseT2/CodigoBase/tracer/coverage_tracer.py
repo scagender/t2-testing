b50202de904d73a3dac46596184aaafb7daeb435
ercaceres <ercaceres@uc.cl>
2024-05-06 16:57:52 -0400

    tracer completado

--- CodigoBaseT2/CodigoBase/tracer/coverage_tracer.py
+++ CodigoBaseT2/CodigoBase/tracer/coverage_tracer.py
@@ -4,6 +4,7 @@ import inspect
 from types import *
 import traceback
 from stack_inspector import StackInspector
+from line_record import LineRecord
 
 """ Clase para la Tarea 2. Para su uso, considere:
 with CoverageTracer() as covTracer:
@@ -14,18 +15,40 @@ covTracer.report_executed_lines()
 
 class CoverageTracer(StackInspector):
 
+ 
     def __init__(self):
         super().__init__(None, self.traceit)
-        # Completa el codigo necesario
+        self.executed_lines = {} 
 
-    # Completa la funcion de rastreo
     def traceit(self, frame, event: str, arg):
-        pass
+        if self.our_frame(frame) or self.problematic_frame(frame):
+            pass
+        else:
+            key = (frame.f_code.co_name, frame.f_lineno)
+            if key not in self.executed_lines:
+                self.executed_lines[key] = LineRecord(*key)
+            else:
+                self.executed_lines[key].increaseFrequency()
+            return self.traceit
+        
+    def traceit(self, frame, event: str, arg):
+        if self.our_frame(frame) or self.problematic_frame(frame):
+            pass
+        else:
+            if event == "line":
+                key = (frame.f_code.co_name, frame.f_lineno)
+                if key not in self.executed_lines:
+                    self.executed_lines[key] = LineRecord(*key)
+                else:
+                    self.executed_lines[key].increaseFrequency()
+        return self.traceit
 
     def print_lines_report(self):
         print("{:<30} {:<10} {:<10}".format('fun', 'line', 'freq'))
-        # Completa el codigo necesario
+        for line_record in self.executed_lines.values():
+            line_record.print_report()
 
     def report_executed_lines(self):
         self.print_lines_report()
-        # Completa el codigo necesario
+        sorted_lines = sorted(self.executed_lines.values(), key=lambda x: x.lineNumber)
+        return sorted_lines
